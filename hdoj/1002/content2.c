2dd1113bce14b040dd33ef94b090821e06a82627
ewnf <958102068@qq.com>
2018-03-01 04:56:51 +0000

    test right,havn't submit.

--- hdoj/1002/content2.c
+++ hdoj/1002/content2.c
@@ -1,7 +1,12 @@
 #include<stdio.h>
-#include<string,h>
+#include<string.h>
 
-void clean_line(char *line){
+int charToint(char aim){
+	if(aim=='\0'){return 0;}
+	else {return (int)(aim-'0');}
+}
+
+void clear_line(char *line){
 	int length;
 	for(length=1001;length>0;length--){
 		line[length]='\0';
@@ -9,46 +14,63 @@ void clean_line(char *line){
 }
 
 void turn(char *line){
-	int len=strlen(line);
-	int li_c;
+	int Len=strlen(line);
+	int digit;
 	char mid;
-	for(li_c=0;li_c<len/2;li_C++){
-		mid=line[li_c];
-		line[li_c]=line[len-1-li_c];
-		line[len-li_c-1]=mid;
+	for(digit=0;digit<Len/2;digit++){
+		mid=line[digit];
+		line[digit]=line[Len-1-digit];
+		line[Len-digit-1]=mid;
 	}
 }
 
-void add(char *lia,char *lib,int res){
+int add(char *lia,char *lib,int *res){
 	int digit=0,carry=0;
 	for(digit=0;digit<1001;digit++){
-		res[digit]=(lia[digit]='\0'?0:lia[digit]-'0')+(lib[digit]='\0'?0:lib[digit]-'0')+carry;
-		if(res[digit]>0){
+		res[digit]=charToint(lia[digit])+charToint(lib[digit])+carry;
+		//printf("%d\t%d\n",res[digit],carry);
+		if(res[digit]>9){
 			res[digit]-10;
 			carry=1;
 		}else{carry=0;}
-		if(lia[digit]||lib[digit]||carry){
-			break;
-		}
-			
 
+		if( lia[digit]=='\0'
+		&&	lib[digit]=='\0'
+		&&  carry==0       ){break;}
+		
+	}
+	return digit;
 
+}
+	
 int main(){
 	int la=0,lb=0;
 	char a[1001]={0},b[1001]={0};
 	char ta[1001]={0},tb[1001]={0};
-	char res[1001]={0};
-	int turn_nu;
-	scanf("%d",&turn1_nu);
-	int ti_turn1=0;
-	for(ti_turn1=0;ti_turn1<turn1_nu;ti_turn1++){
+	int res[1001]={0};
+	int lor=0;//length of result[1001];
+	int co_turn1=0,Ra_turn1;
+	scanf("%d",&Ra_turn1);
+	for(co_turn1=0;co_turn1<Ra_turn1;co_turn1++){
 		clear_line(a);
 		clear_line(b);
 		scanf("%s %s",a,b);
+		printf("Case %d:\n%s + %s = ",co_turn1+1,a,b);
 		memcpy(ta,a,1001);
-		mencpy(tb,b,1001);
+		memcpy(tb,b,1001);
 		turn(ta);turn(tb);
-		add(ta,tb,res);
+		//printf("\n%s %s\n",ta,tb);
+		lor=add(ta,tb,res);
+		int Co_Turn2=0;
+		for(Co_Turn2=lor;Co_Turn2>0;Co_Turn2--){
+			printf("%d",res[Co_Turn2-1]);
+		}
+		if(co_turn1<Ra_turn1-1){printf("\n\n");}
+		else{printf("\n");}
+	}
+	return 0;
+}
+			
 		
 
 	
