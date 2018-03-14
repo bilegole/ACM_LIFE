94170c5a2628d1c0fec0160a0a05d6d31fad023d
ewnf <958102068@qq.com>
2018-03-14 11:24:43 +0000

    add some code in C_class/4/con.c

--- C_class/2011-D1-final/4/con.c
+++ C_class/2011-D1-final/4/con.c
@@ -10,17 +10,36 @@ struct Student in(){
 	scanf("%d",&student.score);
 	return student;
 }
-	
+
+struct Context {
+	struct Student stu[10000];
+	int stunum,stusum,stuave;
+}	
+
 int main(){
-	int Sum=0,i;
-	struct Student students[10000];
-	for(i=0;;i++){
-		student[i]=in();
-		if(student.id=0){break;}
-		sum++;
-	}
-	int pin=0;
-	for(i=0;i<Sum;i++){
+	struct Context con;
+	con.stunum=0;
+	do{
+		scanf("%d %d",&con.stu[con.stunum].score,&con.stu[con.stunum].id);
+		con.stunum++;
+	}while(con.stu[con.stunum-1].score!=0&&con.stu[con.stunum-1].id!=0);
+	int co_turn1=0,Ra_turn1=con.stuNum=con.stunum;con.stunum-=1;
+	for(con.stusum=0,co_turn1=0;co_turn1<Ra_turn1;co_turn1++){
+		con.stusum+=con.stu[co_turn1].score;
+	}con.stuave=(int)con.stusun/con.stuNum;
+	//sorting students by the score and id
+	int co_turn1=0,Ra_turn1=con.stunum
+	for(
+//	int Stunum=0,i,sum=0;
+//	struct Student students[10000];
+//	for(i=0;;i++){
+//		student[i]=in();
+//		if(student[i].id=0){break;}
+//		Stunum++;
+//	}
+//	int pin=0;
+//	for(i=0;i<Stunum;i++){
+		
 		
 	
 	
