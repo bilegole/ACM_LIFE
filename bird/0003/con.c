d80f8bd934d69dc5bf4aaa7894681a1177e894f1
ewnf <958102068@qq.com>
2018-03-15 11:44:21 +0000

    finish the forth problem!

--- bird/0003/con.c
+++ bird/0003/con.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
+#define isTest 1
 
 int main(){
 	//get the frofit.
 	double profit,bonus;
 	int rank[6]={0,10,20,40,60,100};
-	double rate[6]={0.2,0.1,0.5,0.4,0.3,0.1};
-	scanf("%d",&frofit);
+	double rate[6]={0.2,0.1,0.05,0.04,0.03,0.01};
+	scanf("%lf",&profit);
+	if(isTest==1){printf("My profit is %lf,",profit);} 
 	//calculate bonus
 	int co;
 	for(co=1;co<5;co++){
@@ -17,6 +19,9 @@ int main(){
 			profit=0;
 			break;
 		}
+	}
 	if(profit>0){bonus+=profit*rate[5];profit=0;}
-	printf("
+	printf("And my bouns is %lf!\n",bonus);
+	return 0;
+}
 			
