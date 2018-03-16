ae98d85c68c66fddd3601058efa882bfcb1600c6
ewnf <958102068@qq.com>
2018-03-16 02:41:16 +0000

    add the second version in 0012

--- bird/0012/2con.c
+++ bird/0012/2con.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 
 int main(){
-	int co1,Ra1;
-	for(co1=
+	int co1,co2,tig=1;//tig=1->isPrime
+	for(co1=101;co1<201;co1++){
+		for(tig=1,co2=2;co2<co1;co2++){
+			if(co1%co2==0){tig=0;}
+		}if(tig){printf("%d\tis Prime\n",co1);}
+	}
+	return 0;
+}
