89aaddcbb99154ed5c38c500f7da1799aff38ef0
ewnf <958102068@qq.com>
2018-04-22 09:55:54 +0000

    finish + and - , is testing *

--- C_class/上机/算法类/2/copy/con.c
+++ C_class/上机/算法类/2/copy/con.c
@@ -1,8 +1,10 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
+#include<math.h>
 #define Len 1000
 #define isTest 1
+
 
 typedef struct  {
 	int *con,len,sig;	//sig=1 -> 正数		sig=0 -> 负数
@@ -13,6 +15,14 @@ typedef struct  {
 	int *sig;			//[0]代表运算种类，[1]代表较大数位置
 	Num *res;
 }Pro;
+
+int power(int a,int x){
+	int res=1;
+	for(int i=0;i<x;i++){
+		res*=a;
+	}
+	return res;
+}
 
 int bigger(Num *num1,Num *num2){
 	if(num1->sig>0&&num2->sig<0){return 1;}
@@ -107,6 +117,31 @@ char *Num_to_str(Num *tar){
 	return res;
 }
 
+//将int对象转化为Num对象。
+Num *int_to_Num(int tar){
+	int digit=0;
+	Num *num=(Num*)calloc(1,sizeof(Num));
+	num->con=(int*)calloc(Len,sizeof(Num));
+	if(tar<0){num->sig=0;}
+	else{num->sig=1;}
+	for(digit=0;tar>0;digit++){
+		num->con[digit]=tar%10;
+		tar/=10;
+	}
+	num->len=digit;
+	return num;
+}	
+
+//将Num对象转化为int对象（可能会爆表）。
+int Num_to_int(Num *num){
+	int tar=0;
+	for(int digit=0;digit<Len;digit++){
+		tar+=num->con[digit]*power(10,digit);
+	}
+	tar*=num->sig=1?1:-1;
+	return tar;
+}
+
 //初始化pro对象
 Pro *pro_creat(int sig){							
 	Pro *pro=(Pro*)calloc(1,sizeof(Pro));			//分配pro空间
@@ -122,17 +157,18 @@ Pro *pro_creat(int sig){
 }
 
 //将num对象写入pro中。
-void num_to_pro(Pro *pro,Num *num,int dig){
+void Num_to_pro(Pro *pro,Num *num,int dig){
 	pro->nums[dig]=*num;
 }
 
 //pro的正式运算
 void pro_start(Pro *pro){
+	int bi,sm;
 	pro->sig[1]=num_abs_compare(pro->nums,pro->nums+1);
 	if(pro->sig[0]<2){
 		if(pro->sig[0]!=(pro->nums[0].sig*pro->nums[1].sig)){
 			//ba_add(pro->nums[0],pro->nums[1]);
-			printf("开始加法。对象为%s和%s\n",Num_to_str(pro->nums),Num_to_str(pro->nums+1));
+			//printf("开始加法。对象为%s和%s\n",Num_to_str(pro->nums),Num_to_str(pro->nums+1));
 //			pro->res=str_to_Num("3999");
 			int tig=0;
 			for(int digit=0;digit<Len;digit++){
@@ -148,20 +184,19 @@ void pro_start(Pro *pro){
 			pro->res->len=(pro->sig[1]==1)?pro->nums[0].len:pro->nums[1].len;
 		}else{
 			//ba_dec(pro->nums[0],pro->nums[1]);
-			printf("开始减法。对象为%s和%s\n",Num_to_str(pro->nums),Num_to_str(pro->nums+1));
+			//printf("开始减法。对象为%s和%s\n",Num_to_str(pro->nums),Num_to_str(pro->nums+1));
 			if(pro->sig[1]==0){
 				pro->res=str_to_Num("0");
 			}else{
 				if(pro->sig[1]==1){
-					int bi=0,sm=1;
+					bi=0,sm=1;
 					pro->res->sig=pro->nums->sig;
 				}else if(pro->sig[1]==2){
-					int bi=1,sm=0;
+					bi=1,sm=0;
 					pro->res->sig=pro->nums->sig==1?0:1;
 				}
 				for(int digit=0,tig=0;digit<Len;digit++){
-					pro->res->con[digit]=pro->nums[1].con[digit]-pro->nums[0].con[digit]-tig;
-					//此处需要修正
+					pro->res->con[digit]=pro->nums[bi].con[digit]-pro->nums[sm].con[digit]-tig;
 					if(pro->res->con[digit]<0){
 						pro->res->con[digit]+=10;
 						tig=1;
@@ -169,21 +204,64 @@ void pro_start(Pro *pro){
 						tig=0;
 					}
 				}
+				int len=0;
+				for(len=Len;pro->res->con[len-1]==0;len--){}
+				pro->res->len=len;
+					
 			}
 		}
 	}else if (pro->sig[0]==2){
 		printf("开始乘法");
+		if(pro->nums[0].sig==pro->nums[1].sig){
+			pro->res->sig=1;
+		}else{pro->res->sig=0;}
+		for(int a=0;a<Len;a++){
+			pro->res->con[a]=0;
+		}
+		for(int a=0;a<Len;a++){
+			for(int b=0;b<Len;b++){
+				int mid=0;int tig=a+b;
+				pro->res->con[tig]+=pro->nums[0].con[a]*pro->nums[1].con[b];
+				while(pro->res->con[tig]>9){
+					mid=pro->res->con[tig]/10;
+					pro->res->con[tig]%=10;
+					pro->res->con[++tig]+=mid;
+				}
+			}
+		}
 	}else if (pro->sig[0]==3){
 		printf("开始除法");
 	}
 }
 
+void test6(){
+	
+}
+
+void test5(){
+	for(int a=0;a<1000;a++){
+	for(int b=0;b<1000;b++){
+		Pro *pro=pro_creat(0);
+		Num_to_pro(pro,int_to_Num(a),0);
+		Num_to_pro(pro,int_to_Num(b),1);
+		pro_start(pro);
+		if(Num_to_int(pro->res)!=a+b){
+			printf("好像错了？\n");
+		}
+	}}
+}
+
+void test4(){
+	printf("1000 is %s\n",Num_to_str(int_to_Num(1000)));	
+	printf("1000 is %d\n",Num_to_int(str_to_Num("1000")));
+}
+
 void test3(){
-	char *one="1300";
-	char *two="2999";
- 	Pro *pro=pro_creat(0);
-	num_to_pro(pro,str_to_Num(one),0);
-	num_to_pro(pro,str_to_Num(two),1);
+	char *one="1000";
+	char *two="1";
+ 	Pro *pro=pro_creat(1);
+	Num_to_pro(pro,str_to_Num(one),0);
+	Num_to_pro(pro,str_to_Num(two),1);
 	pro_start(pro);
 	printf("%s\n",Num_to_str(pro->res));
 }
@@ -209,6 +287,6 @@ void test1(){
 }
 
 int main(){
-	test3();
+	test6();
 	return 0;
 }
